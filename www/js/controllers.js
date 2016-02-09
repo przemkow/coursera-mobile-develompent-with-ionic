a3696a9626df7f6f8f94b0301007810ecb0afe39
przemkow <przemkow92@gmail.com>
2016-02-09 14:40:06 +0100

    user remember with local storage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('conFusion.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $localStorage) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -10,7 +10,7 @@ angular.module('conFusion.controllers', [])
   //});
 
   // Form data for the login modal
-  $scope.loginData = {};
+  $scope.loginData = $localStorage.getObject('userinfo','{}');
 
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
@@ -32,6 +32,7 @@ angular.module('conFusion.controllers', [])
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
+    $localStorage.storeObject('userinfo',$scope.loginData);
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
--- www/js/services.js
+++ www/js/services.js
@@ -58,4 +58,21 @@ angular.module('conFusion.services', ['ngResource'])
             return favFac;
         }])
 
+        .factory('$localStorage', ['$window', function($window) {
+          return {
+            store: function(key, value) {
+              $window.localStorage[key] = value;
+            },
+            get: function(key, defaultValue) {
+              return $window.localStorage[key] || defaultValue;
+            },
+            storeObject: function(key, value) {
+              $window.localStorage[key] = JSON.stringify(value);
+            },
+            getObject: function(key,defaultValue) {
+              return JSON.parse($window.localStorage[key] || defaultValue);
+            }
+          }
+        }])
+
 ;
