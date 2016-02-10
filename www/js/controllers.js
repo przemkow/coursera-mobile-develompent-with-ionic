fb9f44036eff2d540088c2e35d710ad9f2fb50df
przemkow <przemkow92@gmail.com>
2016-02-10 01:43:25 +0100

    add registration form with posibility to take a picture

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('conFusion.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, $localStorage) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $localStorage, $ionicPlatform, $cordovaCamera) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -8,6 +8,30 @@ angular.module('conFusion.controllers', [])
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+
+  $ionicPlatform.ready(function() {
+      var options = {
+          quality: 50,
+          destinationType: Camera.DestinationType.DATA_URL,
+          sourceType: Camera.PictureSourceType.CAMERA,
+          allowEdit: true,
+          encodingType: Camera.EncodingType.JPEG,
+          targetWidth: 100,
+          targetHeight: 100,
+          popoverOptions: CameraPopoverOptions,
+          saveToPhotoAlbum: false
+      };
+       $scope.takePicture = function() {
+          $cordovaCamera.getPicture(options).then(function(imageData) {
+              $scope.registration.imgSrc = "data:image/jpeg;base64," + imageData;
+          }, function(err) {
+              console.log(err);
+          });
+
+          $scope.registerform.show();
+
+      };
+  });
 
   // Form data for the login modal
   $scope.loginData = $localStorage.getObject('userinfo','{}');
@@ -40,6 +64,38 @@ angular.module('conFusion.controllers', [])
       $scope.closeLogin();
     }, 1000);
   };
+
+
+  $scope.registration = {};
+
+  // Create the registration modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/register.html', {
+      scope: $scope
+  }).then(function (modal) {
+      $scope.registerform = modal;
+  });
+
+  // Triggered in the registration modal to close it
+  $scope.closeRegister = function () {
+      $scope.registerform.hide();
+  };
+
+  // Open the registration modal
+  $scope.register = function () {
+      $scope.registerform.show();
+  };
+
+  // Perform the registration action when the user submits the registration form
+  $scope.doRegister = function () {
+      console.log('Doing reservation', $scope.reservation);
+
+      // Simulate a registration delay. Remove this and replace with your registration
+      // code if using a registration system
+      $timeout(function () {
+          $scope.closeRegister();
+      }, 1000);
+  };
+
 
   // Create the reserve modal that we will use later
   $ionicModal.fromTemplateUrl('templates/reserve.html', {
