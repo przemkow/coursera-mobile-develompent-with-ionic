43a3b28339dc621eaa907b0cf82a88d4e1763861
przemkow <przemkow92@gmail.com>
2016-02-10 01:05:16 +0100

    notification and toast popup when adding to favourite

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -70,8 +70,8 @@ angular.module('conFusion.controllers', [])
   };
 })
 
-.controller('MenuController', ['$scope', 'dishes', 'favoriteFactory', 'baseURL', '$ionicListDelegate',
-              function ($scope, dishes, favoriteFactory, baseURL, $ionicListDelegate) {
+.controller('MenuController', ['$scope', 'dishes', 'favoriteFactory', 'baseURL', '$ionicListDelegate', '$ionicPlatform', '$cordovaLocalNotification', '$cordovaToast',
+              function ($scope, dishes, favoriteFactory, baseURL, $ionicListDelegate, $ionicPlatform, $cordovaLocalNotification, $cordovaToast) {
 
     $scope.baseURL = baseURL;
     $scope.tab = 1;
@@ -103,6 +103,27 @@ angular.module('conFusion.controllers', [])
         console.log("index is " + index);
         favoriteFactory.addToFavorites(index);
         $ionicListDelegate.closeOptionButtons();
+
+        $ionicPlatform.ready(function () {
+            $cordovaLocalNotification.schedule({
+                id: 1,
+                title: "Added Favorite",
+                text: $scope.dishes[index].name
+            }).then(function () {
+                console.log('Added Favorite '+$scope.dishes[index].name);
+            }, function () {
+                console.log('Failed to add Notification ');
+            });
+
+            $cordovaToast
+              .show('Added Favorite '+$scope.dishes[index].name, 'long', 'center')
+              .then(function (success) {
+                  // success
+              }, function (error) {
+                  // error
+              });
+        });
+
     };
 }])
 
